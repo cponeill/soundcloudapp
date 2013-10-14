2ef8c589e2c35abfbd515669e58eb90fc9952df0
cponeill <caseyoneill78@hotmail.com>
2013-10-13 17:36:51 -0700

    Create web.js

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var fs = require("fs");
+var path = require("path");
+var htmlfile = "home.html";
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
