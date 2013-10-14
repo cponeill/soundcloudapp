efb4a5088ab89f952d733c5d7896b2144d468bed
cponeill <caseyoneill78@hotmail.com>
2013-10-14 03:31:27 +0000

    making those changes

--- public/script.js
+++ public/script.js
@@ -1,15 +1,26 @@
-SC.initialize({
-  client_id: 'b7c7098351dca4d9ca65af321eeaffae',
-  redirect_url: 'http://www.designingaround.us/callback.html',
-});
+function playSomeSound(genre) {
+    SC.get('/tracks', {
+      genres: genre,
+	bpm: {
+	    from: 100
+	    }
+	}, function(tracks) {
+	    var random = Math.floor(Math.random() * 49);
+	    SC.oEmbed(tracks[random].uri, { auto_play: true }, document.getElementById('target'));
+	});
+}
 
-$(document).ready(function() {
-  $('a.connect').click(function(e) {
-    e.preventDefault();
-    SC.connect(function() {
-      SC.get('/me', function(me) {
-        $('#username').html(me.username);
-      });
-    });
-  });
-});
+window.onload = function() {
+    SC.intialize({
+	client_id: 'b7c7098351dca4d9ca65af321eeaffae'
+	});
+    
+    var menuLinks = document.getElementsByClassName('genre');
+    for (var i = 0; i < menuLinks.length; i++)
+	var menuLink = menuLinks[i];
+        menuLinks.onclick = function(e) {
+	e.preventDefault();
+	    playSomeSound(menuLinks.innerHTML);
+	};
+	}
+}
