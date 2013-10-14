3fca0570aeb27cd953e6deac0fafe9bc23acc41d
cponeill <caseyoneill78@hotmail.com>
2013-10-14 02:43:49 +0000

    adding stuff

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-SC.intialize({
-    client_id: 'b7c7098351dca4d9ca65af321eeaffae'
-    redirect_url: 'http://www.designingaround.us'
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4',
+  redirect_uri: 'http://external.codecademy.com/soundcloud.html'
 });
 
 $(document).ready(function() {
